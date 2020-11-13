b73cc9aeaf9c709694fd3294e0d5dd58a54852e2
Aggelos Karapatakis <63917743+Karapatakis106@users.noreply.github.com>
2020-11-13 21:31:23 +0200

    Add files via upload

---
+++ MakeMeBootable.sh
@@ -0,0 +1,49 @@
+# /bin/bash
+
+echo "********************************************************************"
+echo "*                                                                  *"
+echo "*                  MAKE ME BOOTABLE!                               *"
+echo "*                                                                  *"
+echo "*                   {Version 0.1}                                  *"
+echo "********************************************************************"
+
+echo "Now the list of connected devices will appear.."; sleep 5;
+sudo fdisk -l
+sleep 3;
+echo "Select the drive that you want to format and make bootable your iso: ** WARNING THIS SELECTED DRIVE WILL BE FORMATED!!!"; sleep 10;
+  read ADDRESS
+
+FILE=`zenity --file-selection --title="Select an ISO File"`
+
+case $? in
+         0)
+                echo "\"$FILE\" selected.";;
+         1)
+                echo "No file selected.";;
+        -1)
+                echo "An unexpected error has occurred.";;
+esac
+(
+echo "10" ; sleep 1
+echo "# Getting Ready" ; sleep 25
+echo "20" ; sleep 1
+echo "# Get Root Access.." ; sleep 10
+sudo-i
+echo "50" ; sleep 1
+echo "# Writing Image..." ; sleep 120
+cd $ADDRESS && rm -rf* && sudo dd if=$FILE of=$ADDRESS bs=1M;
+echo "75" ; sleep 1
+echo "# Finishing and Unmounting Selected device..." ; sleep 6
+sudo unmount $ADDRESS
+echo "100" ; sleep 4
+echo "# Finished!"
+) |
+zenity --progress \
+  --title="Writing Image.." \
+  --text="Version 0.01" \
+  --percentage=0
+
+if [ "$?" = -1 ] ; then
+        zenity --error \
+          --text="An error occurred during writing"
+fi
